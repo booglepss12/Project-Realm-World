daeeae427f3162395a5689f2b00ea2a2308791c1
Unknown <grooney4@gmail.com>
2018-01-21 16:34:42 -0500

    Created path, updated naming scheme in code, added temp enemy

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -19,7 +19,9 @@ public class CubeEditor : MonoBehaviour {
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
         textMesh = GetComponentInChildren<TextMesh>();
-        textMesh.text = snapPos.x/gridSize + " , " + snapPos.z/gridSize;
+        string labelText = snapPos.x/gridSize + " , " + snapPos.z/gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
        
 	}
 }
