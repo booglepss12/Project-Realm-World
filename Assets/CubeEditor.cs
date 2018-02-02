eded9f72c375aca4e151d1c04c57c383d21b9521
Unknown <grooney4@gmail.com>
2018-02-01 20:05:39 -0500

    Added dictionary in script to index path

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,25 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
 public class CubeEditor : MonoBehaviour {
-    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
-    TextMesh textMesh;
-
     
+   
+    Vector3 gridPos;
+    Waypoint waypoint;
 
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
     // Update is called once per frame
     void Update ()
     {
-        Vector3 snapPos;
-        
-        
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
-        textMesh = GetComponentInChildren<TextMesh>();
-        string labelText = snapPos.x/gridSize + " , " + snapPos.z/gridSize;
+        SnapToGrid();
+        UpdateLabel();
+
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x,
+            0f, 
+           waypoint.GetGridPos().y
+            );
+    }
+
+    private void UpdateLabel()
+    {
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        int gridSize = waypoint.GetGridSize();
+        string labelText = waypoint.GetGridPos().x / gridSize 
+            + " , " 
+            + waypoint.GetGridPos().y/ gridSize;
         textMesh.text = labelText;
         gameObject.name = labelText;
-       
-	}
+    }
 }
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint: MonoBehaviour {
+    const int gridSize = 10;
+    Vector2Int gridPos;
+    // Use this for initialization
+    
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public Vector2Int GetGridPos()
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
+            Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+            );
+    }
 	// Update is called once per frame
 	void Update () {
 		
