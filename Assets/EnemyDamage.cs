41c8214d265fc04dd8a09aadc74afd4225b25dfc
Unknown <grooney4@gmail.com>
2018-03-01 16:46:04 -0500

    Enemy now has hit points and can be destroyed

--- Assets/EnemyDamage.cs
+++ Assets/EnemyDamage.cs
@@ -1,16 +1,69 @@
-﻿using System.Collections;
+﻿using System;
+
+using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class EnemyDamage : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+public class EnemyDamage : MonoBehaviour
+{
+
+
+
+    [SerializeField] Collider collisionMesh;
+
+    [SerializeField] int hitPoints = 10;
+
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+
+
+
+    }
+
+    private void OnParticleCollision(GameObject other)
+
+    {
+
+        ProcessHit();
+
+        if (hitPoints <= 1)
+
+        {
+
+            KillEnemy();
+
+        }
+
+    }
+
+
+
+    void ProcessHit()
+
+    {
+
+        hitPoints = hitPoints - 1;
+
+        print("current hitpoints are " + hitPoints);
+
+    }
+
+
+
+    private void KillEnemy()
+
+    {
+
+        Destroy(gameObject);
+
+    }
+
 }
