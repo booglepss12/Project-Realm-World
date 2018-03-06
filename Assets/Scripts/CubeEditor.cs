e8ceeaa2ffc315e9c6fcc9f825d722913a648d6a
Unknown <grooney4@gmail.com>
2018-03-05 21:40:45 -0500

    Enemies now spawning

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -1,44 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [ExecuteInEditMode]
 [SelectionBase]
 [RequireComponent(typeof(Waypoint))]
 public class CubeEditor : MonoBehaviour {
-    
-   
-    Vector3 gridPos;
+
     Waypoint waypoint;
 
     private void Awake()
     {
         waypoint = GetComponent<Waypoint>();
     }
-    // Update is called once per frame
-    void Update ()
+
+    void Update()
     {
         SnapToGrid();
         UpdateLabel();
-
     }
 
     private void SnapToGrid()
     {
         int gridSize = waypoint.GetGridSize();
         transform.position = new Vector3(
-            waypoint.GetGridPos().x*gridSize,
-            0f, 
-           waypoint.GetGridPos().y*gridSize
-            );
+            waypoint.GetGridPos().x * gridSize,
+            0f,
+            waypoint.GetGridPos().y * gridSize
+        );
     }
 
     private void UpdateLabel()
     {
         TextMesh textMesh = GetComponentInChildren<TextMesh>();
         string labelText = 
-            waypoint.GetGridPos().x 
-            + " , " 
-            + waypoint.GetGridPos().y;
+            waypoint.GetGridPos().x +
+            "," + 
+            waypoint.GetGridPos().y;
         textMesh.text = labelText;
         gameObject.name = labelText;
     }
