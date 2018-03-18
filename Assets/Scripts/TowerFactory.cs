c2dd3d36a6ec8171c5888510f0a100a5621ea558
Unknown <grooney4@gmail.com>
2018-03-18 16:54:24 -0400

    Added a TowerFactory.cs script that limits the number of towers placed

---
+++ Assets/Scripts/TowerFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFactory : MonoBehaviour {
+    [SerializeField] Tower towerPrefab;
+    [SerializeField] int towerLimit = 5;
+    int numTowers = 0;
+    // Use this for initialization
+    public void AddTower(Waypoint baseWaypoint)
+    {
+        if (numTowers < towerLimit)
+        {
+            InstantiateNewTower(baseWaypoint);
+        }
+        else
+        {
+            MoveExistingTower();
+        }
+
+    }
+
+    private static void MoveExistingTower()
+    {
+        print("Tower Limit has been reached");
+    }
+
+    private void InstantiateNewTower(Waypoint baseWaypoint)
+    {
+        Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity);
+        baseWaypoint.isPlaceable = false;
+        numTowers++;
+    }
+}
