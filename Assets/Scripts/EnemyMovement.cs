3160d948a849fb08cc2f1d9121552f3376d15bb7
Unknown <grooney4@gmail.com>
2018-03-24 18:16:44 -0400

    Added particle system for finishing enemies, scoreboard, and particles

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
-
+    [SerializeField] float movementPeriod = 0.5f;
+    [SerializeField] ParticleSystem goalParticle;
 	// Use this for initialization
 	void Start () {
         Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
@@ -18,8 +19,18 @@ public class EnemyMovement : MonoBehaviour {
         foreach (Waypoint waypoint in path)
         {
             transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(movementPeriod);
         }
         print("Ending patrol");
+        SelfDestruct();
+    }
+    private void SelfDestruct()
+    {
+
+        var vfx = Instantiate(goalParticle, transform.position, Quaternion.identity);
+        vfx.Play();
+        float destroyDelay = vfx.main.duration;
+        Destroy(vfx.gameObject, destroyDelay);
+        Destroy(gameObject);
     }
 }
